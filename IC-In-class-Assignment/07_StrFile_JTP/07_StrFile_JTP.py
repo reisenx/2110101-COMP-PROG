3d7bf1409dae9e7e500613ee487a48f9a0c54c0f
Worralop Srichainont <worralop.sric@gmail.com>
2025-08-27 14:45:50 +0700

    feat: add 07_strfile_jtp

--- IC-In-class-Assignment/07_StrFile_JTP/07_StrFile_JTP.py
+++ IC-In-class-Assignment/07_StrFile_JTP/07_StrFile_JTP.py
@@ -1,16 +1,21 @@
-# Algorithm
-# 1.) split() the text with comma (,) first
-# 2.) strip() each item in the list to delete the excess spacebar
-# 3.) Modify each item in the list. Make the first letter as uppercase and the rest are lower case
-# 4.) output using .join()
-# Example: text = "Tom, aNN,      TONY,jOe"
-# 1.) text.split(',') = ['Tom',' aNN','      TONY','jOe']
-# 2.) strip() each item in the list --> ['Tom','aNN','TONY','jOe']
-# 3.) Modify each item in the list --> ['Tom','Ann','Tony','Joe']
-# 4.) Output using " ".join(list) --> "Tom Ann Tony Joe"
-text = str(input())
-text_list = text.split(',')
-for i in range(len(text_list)):
-    text_list[i] = text_list[i].strip()
-    text_list[i] = text_list[i][0].upper() + text_list[i][1:].lower()
-print(" ".join(text_list))
+# --------------------------------------------------
+# File Name : 07_StrFile_JTP.py
+# Problem   : Capitalization
+# Author    : Worralop Srichainont
+# Date      : 2025-08-14
+# --------------------------------------------------
+
+# Input a list of words
+raw_words = input().strip().split(",")
+
+# Initialize a list to store formatted words
+words = []
+
+# Format each word, and store it to a list
+for raw_word in raw_words:
+    word = raw_word.strip().lower()
+    word = f"{word[0].upper()}{word[1:]}"
+    words.append(word)
+
+# Output
+print(" ".join(words))
