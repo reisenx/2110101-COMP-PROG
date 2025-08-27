f126a5a243965eb9ee7b4f86f54cf6a15d6b8f9c
Worralop Srichainont <worralop.sric@gmail.com>
2025-08-27 14:45:22 +0700

    feat: add 05_list_jtp

--- IC-In-class-Assignment/05_List_JTP/05_List_JTP.py
+++ IC-In-class-Assignment/05_List_JTP/05_List_JTP.py
@@ -1,21 +1,26 @@
-# Input a string and convert it to a list of float
-# Example: "20 50 90 90" --> [20.0, 50.0, 90.0, 90.0]
-number = [float(e) for e in input().split()]
+# --------------------------------------------------
+# File Name : 05_List_JTP.py
+# Problem   : Interpolation
+# Author    : Worralop Srichainont
+# Date      : 2025-08-14
+# --------------------------------------------------
 
-# Create a new list to insert an average of number i and i+1
-# Example: [1.0, 3.0, 5.0, 7.0, 8.0] 
-# Loop 1: (1.0 + 3.0)/2 = 2.0, insert 1.0 and 2.0 into a new list
-# Loop 2: (3.0 + 5.0)/2 = 4.0, insert 3.0 and 4.0 into a new list
-# Loop 3: (5.0 + 7.0)/2 = 6.0, insert 5.0 and 6.0 into a new list
-# Loop 4: (7.0 + 8.0)/2 = 7.5, insert 7.0 and 7.5 into a new list
-# After for loop, insert 8.0 into a list
-# Output: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 7.5, 8.0]
-interpolation = []
-for i in range (0,len(number)-1):
-    interpolation.append(number[i])
-    interpolation.append((number[i] + number[i+1]) / 2)
-# Insert a last number
-interpolation.append(number[-1])
+# Input numbers a list of float
+numbers = [float(num) for num in input().split()]
 
-# Output
-print(interpolation)
+# Initialize after number interpolation process
+interpolated_numbers = []
+
+# Interpolate the numbers by inserting the average of each pair
+for i in range(len(numbers) - 1):
+    # Calculate the average of the current and next number
+    avg_value = (numbers[i] + numbers[i + 1]) / 2
+
+    # Append the current number and the average to the new list
+    interpolated_numbers.append(numbers[i])
+    interpolated_numbers.append(avg_value)
+# Append the last number
+interpolated_numbers.append(numbers[-1])
+
+# Output the interpolated numbers
+print(interpolated_numbers)
