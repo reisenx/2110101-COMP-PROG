e9d113118dd1c53f6fe4210899cf57ee2399eb67
Worralop Srichainont <worralop.sric@gmail.com>
2025-08-27 14:44:28 +0700

    feat: add 02_strlist_jtp solution

--- IC-In-class-Assignment/02_StrList_JTP/02_StrList_JTP.py
+++ IC-In-class-Assignment/02_StrList_JTP/02_StrList_JTP.py
@@ -1,22 +1,14 @@
-# Input string
-txt = str(input())
+# --------------------------------------------------
+# File Name : 02_StrList_JTP.py
+# Problem   : Name and Birthday
+# Author    : Worralop Srichainont
+# Date      : 2025-08-14
+# --------------------------------------------------
 
-# Split txt with ',' and get surname from index 0
-# Ex. "Gibb,Robin:22/December/1949" --> ["Gibb", "Robin:22/December/1949"]
-data1 = txt.split(",")
-surname = data1[0]
+# Extract name and birthday from the input
+raw_name, raw_date = input().strip().split(":")
+surname, name = raw_name.split(",")
+day, month, year = raw_date.split("/")
 
-# Split data1[1] with ':' and get name from index 0
-# Ex. data1[1] = "Robin:22/December/1949"
-#     "Robin:22/December/1949" --> ["Robin", "22/December/1949"]
-data2 = data1[1].split(":")
-name = data2[0]
-
-# Split data2[1] with "/" and get day, month and year
-# Ex. data2[1] = 22/December/1949
-#     "22/December/1949" --> ["22","December","1949"]
-data3 = data2[1].split("/")
-day,month,year = data3[0], data3[1], data3[2]
-
-# Output in the specific format
-print(name, surname + ":", month, day + ",", year)
+# Output the formatted string
+print(f"{name} {surname}: {month} {day}, {year}")
