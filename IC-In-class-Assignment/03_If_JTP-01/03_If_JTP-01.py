eb161ee0928d22e31cf988c97f7c4c4e7dd858ef
Worralop Srichainont <worralop.sric@gmail.com>
2025-08-27 14:44:47 +0700

    feat: add 03_if_jtp-01

--- IC-In-class-Assignment/03_If_JTP-01/03_If_JTP-01.py
+++ IC-In-class-Assignment/03_If_JTP-01/03_If_JTP-01.py
@@ -1,39 +1,38 @@
-# Input a string
-txt1 = str(input())
-txt2 = str(input())
+# --------------------------------------------------
+# File Name : 03_If_JTP-01.py
+# Problem   : Older
+# Author    : Worralop Srichainont
+# Date      : 2025-08-14
+# --------------------------------------------------
 
-# Spilt data to get data
-# 1.) Use split() to split data into name and date
-# Example: Jane 23/3/2543 --> ["Jane", "23/3/2543"]
-split1_1 = txt1.split()
-split2_1 = txt2.split()
-name1 = split1_1[0]
-name2 = split2_1[0]
-# 2.) Use split("/") to split date into day, month and year
-# Example: 23/3/2543 --> ["23","3","2543"]
-split1_2 = split1_1[1].split("/")
-split2_2 = split2_1[1].split("/")
-day1, month1, year1 = int(split1_2[0]), int(split1_2[1]), int(split1_2[2])
-day2, month2, year2 = int(split2_2[0]), int(split2_2[1]), int(split2_2[2])
+# List of month names
+MONTH = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
 
-# Compare and Output who is older
-# Check the year of brithday
-if(year1 == year2):
-    # If in the same year, check the month of birthday
-    if(month1 == month2):
-        # If in the same month, check the day of birthday
-        # If both birthday is the same, print both
-        if(day1 == day2):
-            print(name1, name2)
-        elif(day1 < day2):
-            print(name1)
-        elif(day2 < day1):
-            print(name2)
-    elif(month1 < month2):
-        print(name1)
-    elif(month2 < month1):
-        print(name2)
-elif(year1 < year2):
-    print(name1)
-elif(year2 < year1):
-    print(name2)
+# Input first person's name and birthday
+name01, m1, d1, y1 = input().strip().split()
+birthday01 = [int(y1), MONTH.index(m1) + 1, int(d1.strip(","))]
+
+# Input second person's name and birthday
+name02, m2, d2, y2 = input().strip().split()
+birthday02 = [int(y2), MONTH.index(m2) + 1, int(d2.strip(","))]
+
+# Compare the two birthdays and output the older person
+if birthday01 < birthday02:
+    print(name01)
+elif birthday01 > birthday02:
+    print(name02)
+else:
+    print(name01, name02)
